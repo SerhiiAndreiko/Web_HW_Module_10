ea0e564ee24e9484782493075491a4bd3f04c31a
SerhiiAndreiko <130772110+SerhiiAndreiko@users.noreply.github.com>
2023-11-14 18:33:49 +0100

    Update code

---
+++ hw10_project/quotes/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+app_name = "quotes"
+
+urlpatterns = [
+    path("", views.main, name='root'),
+    path("<int:page>", views.main, name='root_paginate'),
+    path('add_author/', views.add_author, name='add_author'),
+    path('add_quote/', views.add_quote, name='add_quote'),
+    path('delete/<int:id_>/', views.delete_quote, name='delete_quote'),
+    path('author/<str:id_>/', views.author_detail, name='author_detail'),
+]
---
+++ hw10_project/users/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+
+from .views import RegisterView
+
+from .forms import LoginForm
+
+app_name = "users"
+
+urlpatterns = [
+    path("signup/", RegisterView.as_view(), name='register'),
+    path("login/", LoginView.as_view(template_name='users/signin.html', authentication_form=LoginForm,
+                                     redirect_authenticated_user=True), name='login'),
+    path("logout/", LogoutView.as_view(template_name='users/signout.html'), name='logout'),
+]
